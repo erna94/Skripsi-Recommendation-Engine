fc99921ac8ccc54ef10c5ec3fb6ae9925c36d690
erna94 <erna9470@gmail.com>
2019-05-03 21:20:54 -0500

    chatbot

---
+++ modules/android/ern-chatbot/app/src/main/java/com/example/ernchatbot/Message.java
@@ -0,0 +1,19 @@
+package com.example.ernchatbot;
+
+public class Message {
+    private String text;
+    private boolean belongsToCurrentUser;
+
+    public Message(String text, boolean belongsToCurrentUser) {
+        this.text = text;
+        this.belongsToCurrentUser = belongsToCurrentUser;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isBelongsToCurrentUser() {
+        return belongsToCurrentUser;
+    }
+}
