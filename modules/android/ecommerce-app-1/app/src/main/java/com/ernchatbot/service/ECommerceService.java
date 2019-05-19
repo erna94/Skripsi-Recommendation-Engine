c26ba0f27692a383f4a14cb3732ab2531526937d
erna94 <erna9470@gmail.com>
2019-05-19 14:22:02 -0500

    Added common file

---
+++ modules/android/ecommerce-app-1/app/src/main/java/com/ernchatbot/service/ECommerceService.java
@@ -0,0 +1,8 @@
+package com.ernchatbot.service;
+
+public class ECommerceService {
+    public String getProductByCategory(Long categoryId) {
+
+        return null;
+    }
+}
---
+++ modules/android/ecommerce-app-1/app/src/main/java/com/ernchatbot/service/response/WitEntities.java
@@ -0,0 +1,35 @@
+package com.ernchatbot.service.response;
+
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+public class WitEntities {
+    WitEntity[] subCategoryProduct;
+    WitEntity[] categoryProduct;
+    WitEntity[] intent;
+
+    public WitEntity[] getSubCategoryProduct() {
+        return subCategoryProduct;
+    }
+
+    @JsonSetter("sub_category_produk")
+    public void setSubCategoryProduct(WitEntity[] subCategoryProduct) {
+        this.subCategoryProduct = subCategoryProduct;
+    }
+
+    public WitEntity[] getCategoryProduct() {
+        return categoryProduct;
+    }
+
+    @JsonSetter("category_produk")
+    public void setCategoryProduct(WitEntity[] categoryProduct) {
+        this.categoryProduct = categoryProduct;
+    }
+
+    public WitEntity[] getIntent() {
+        return intent;
+    }
+
+    public void setIntent(WitEntity[] intent) {
+        this.intent = intent;
+    }
+}
