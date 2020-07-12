e992521c093cea2f4297e5d2600aff3b910c4112
erna94 <erna9470@gmail.com>
2020-07-12 20:50:44 +0700

    login task

--- modules/android/ern-chatbot/app/src/main/java/com/example/ernchatbot/ui/LoginTask.java
+++ modules/android/ern-chatbot/app/src/main/java/com/example/ernchatbot/ui/LoginTask.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ListView;
 
+import com.example.ernchatbot.service.LoginService;
+import com.example.ernchatbot.service.LoginService;
 import com.example.ernchatbot.service.response.LoginResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,21 +24,24 @@ public class LoginTask extends AsyncTask<String, Void, LoginResponse> {
 
     @Override
     protected LoginResponse doInBackground(String... message) {
-        String username = message[0];
-        String password = message[1];
+        String username_input = message[0];
+        String password_input = message[1];
 
         LoginResponse loginResponse = new LoginResponse();
         System.out.println(loginResponse.getUserName());
 
         try {
-            username = loginResponse.getUserName();
-            password = loginResponse.getPassword();
+            LoginService loginService= new LoginService();
+            System.out.println(loginService.getLogin("erna", "abc123"));
+
+            username_input = loginResponse.getUserName();
+            password_input = loginResponse.getPassword();
 
             // object ini adalah untuk mengubah dari suatu String berbentuk JSON
             // ke dalam object tertentu. Dalam hal ini kita mau mengubah bentuk
-            // String ke dalam object WitAIResponse
+            // String ke dalam object LoginResponse
             ObjectMapper objectMapper = new ObjectMapper();
-            loginResponse = objectMapper.readValue(username + password, LoginResponse.class);
+            loginResponse = objectMapper.readValue(username_input+ password_input, LoginResponse.class);
 
         } catch(Throwable t) {
             t.printStackTrace();
